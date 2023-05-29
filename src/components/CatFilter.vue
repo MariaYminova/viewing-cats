291375331ff2cd3f119daf5e4c45f42b45c13b33
MariaYminova <vabylia96@gmail.com>
2023-05-29 20:10:18 +0400

    first commit

---
+++ src/components/CatFilter.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div class="cat-filter">
+    <div class="cat-filter__heading">
+      Cat
+      <div class="cat-filter__heading--green">alog</div>
+    </div>
+    <div class="cat-filter__block">Breed</div>
+    <div class="cat-filter__block">Сolor</div>
+    <div class="cat-filter__block">Аge</div>
+  </div>
+
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+
+   .cat-filter {
+    display: flex;
+    align-items: center;
+    margin-bottom: 60px;
+
+    &__heading {
+      font-weight: 700;
+      display: flex;
+      font-size: 56px;
+      margin-right: 50px;
+      color: rgba(234, 179, 84, 1);
+
+      &--green {
+        color: rgba(95, 120, 123, 1);
+      }
+    }
+
+    &__block {
+      background-color: rgba(234, 179, 84, 1);
+      border-radius: 50px;
+      height: 41px;
+      margin-left: 30px;
+      padding: 10px 50px;
+      color: #fff;
+      font-size: 18px;
+      font-weight: 700;
+    }
+  }
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-header">
+    <img src="src/icon/icon-logo.svg" alt="" />
+    <div class="page-header__line"></div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+
+  .page-header {
+    display: flex;
+    justify-content: center;
+    margin: 40px 0 100px 0;
+    position: relative;
+
+    &__line{
+      height: 2px;
+      width: 100%;
+      background-color: rgba(95, 120, 123, 1);
+      position: absolute;
+      top: 51px;
+      z-index: -1;
+    }
+  }
+</style>
